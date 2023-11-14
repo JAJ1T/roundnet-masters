ec8bf6433a9f8bcb4ae5eb274bbc5ef143da69ff
julzel <jusher22@gmail.com>
2023-11-14 16:22:43 -0600

    setup dark-light theme

--- roundnet-masters-cr/src/app/components/Header/index.js
+++ roundnet-masters-cr/src/app/components/Header/index.js
@@ -1,19 +1,26 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useTheme } from '@/app/contexts/ThemeContex';
 import LogoLight from '../../../../public/images/logo-light.png';
+import LogoDark from '../../../../public/images/logo-dark.png';
 
 import styles from './Header.module.scss';
 
 
 const Header = () => {
+  const { theme, toggleTheme } = useTheme();
+  const isDarkTheme = theme === 'dark';
+
   return (
     <header className={styles.header}>
-      <Image src={LogoLight} alt="Logo" width={'auto'} height={100} />
+      <Image src={isDarkTheme ? LogoDark : LogoLight } alt="Logo" width={'auto'} height={100} />
       <nav>
         <Link href="/">Inicio</Link>
         <Link href="/nosotros">¿Quienes somos?</Link>
         <Link href="/roundnet">Roundnet</Link>
         <Link href="/eventos">Eventos</Link>
+        <button onClick={toggleTheme}>Toggle Theme</button>
       </nav>
     </header>
   );
