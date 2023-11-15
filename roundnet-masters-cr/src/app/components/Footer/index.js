630cc8a605ac791eaf160753e9b91a74aa7ef00b
JAJ1T <anthony.avellan2005@gmail.com>
2023-11-14 18:54:38 -0600

    trabajo-footer

--- roundnet-masters-cr/src/app/components/Footer/index.js
+++ roundnet-masters-cr/src/app/components/Footer/index.js
@@ -2,55 +2,33 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div class="box-container">
-        <div class="box">
-          <h5>Informacion del contacto</h5>
-          <a href="#">
-            {' '}
-            <i class="fas fa-envelope"></i> info.roundnetmasters@gmail.com{' '}
-          </a>
-          <a href="#">
-            {' '}
-            <i class="fas fa-phone"></i> Roundnet Masters Costa Rica:
-            +506-7254-4097{' '}
-          </a>
-        </div>
-      </div>
+    <footer>
+			   <section class="footer" id="footer">
 
-      {/*Footer redes sociales*/}
-      <div class="share">
-        <h1>Siguenos en nuestras redes!!!</h1>
-        <br />
-        <a
-          href="#"
-          class="fab fa-facebook-f"
-          onclick="window.open('https://es-la.facebook.com/', '_blank')"
-        ></a>
-        <a
-          href="#"
-          class="fab fa-instagram"
-          onclick="window.open('https://www.instagram.com/roundnetmasters', '_blank')"
-        ></a>
-        <a
-          href="#"
-          class="fab fa-youtube"
-          onclick="window.open('https://www.youtube.com/@RoundnetMasterscr', '_blank')"
-        ></a>
-        <a
-          href="#"
-          class="fab fa-tiktok"
-          onclick="window.open('https://www.tiktok.com/', '_blank')"
-        ></a>
-      </div>
+			   		{/*Footer de informacion del contacto*/}
+				    <div class="box-container">
+				        <div class="box">
+				            <h5>Informacion del contacto</h5>
+				            <a href="#"> <i class="fas fa-envelope"></i> info.roundnetmasters@gmail.com </a>
+				            <a href="#"> <i class="fas fa-phone"></i> Roundnet Masters Costa Rica: +506-7254-4097  </a>
+				        </div>
+				    </div>
 
-      {/*Creditos finales*/}
-      <div class="credit">
-        {' '}
-        Pagina web Oficial de Roundnet Masters Costa Rica | Todos los derechos
-        están reservados!!!{' '}
-      </div>
-    </footer>
+
+				    {/*redes sociales*/}
+				    <div class="share">
+				   		<h1>Siguenos en nuestras redes!!!</h1>
+						<a href="#" class="fab fa-facebook-f" onclick="window.open('https://es-la.facebook.com/', '_blank')"></a>
+						<a href="#" class="fab fa-instagram" onclick="window.open('https://www.instagram.com/roundnetmasters', '_blank')"></a>
+						<a href="#" class="fab fa-youtube"onclick="window.open('https://www.youtube.com/@RoundnetMasterscr', '_blank')"></a>
+						<a href="#" class="fab fa-tiktok"onclick="window.open('https://www.tiktok.com/', '_blank')"></a>
+				    </div>
+
+
+				    	{/*Creditos finales*/}
+				        <div class="credit"> Pagina web Oficial de Roundnet Masters Costa Rica | Todos los derechos están reservados!!! </div>
+				</section>
+			</footer>
   );
 };
 
