5df8d0fe9288f2ca717b5e664357691034ac54c0
AxselOSegueda <147475362+AxselOSegueda@users.noreply.github.com>
2023-11-15 14:03:56 -0600

    estructura de page.js de nosotros

--- roundnet-masters-cr/src/app/nosotros/page.js
+++ roundnet-masters-cr/src/app/nosotros/page.js
@@ -1,9 +1,13 @@
+import Intro from './sections/Intro';
 import Galeria from './sections/Galeria';
+import Outro from './sections/Outro';
 
 const Nosotros = () => {
   return (
     <main>
+      <Intro />
       <Galeria />
+      <Outro />
     </main>
   );
 };
