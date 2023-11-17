130d2ff825a7c5e830104ec26e1aad9c16eb2e7b
AxselOSegueda <147475362+AxselOSegueda@users.noreply.github.com>
2023-11-16 22:27:07 -0600

    dependencias parallax

--- roundnet-masters-cr/src/app/nosotros/sections/Intro/index.js
+++ roundnet-masters-cr/src/app/nosotros/sections/Intro/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import next from 'next/image';
-import styles from './Intro.module.css' 
+import { Parallax } from 'react-parallax';
+import styles from './Intro.module.css';
+import Page from '../../page';
+import image1 from '/roundnet-masters-cr/public/images/nosotros/Inicio.jpg'; 
+import image2 from '/roundnet-masters-cr/public/images/nosotros/equipo.jpg'; 
 
 const Intro = () => {
   return (
-    <section>
-      <div className={styles.intro}>
+    <Parallax
+      blur={10}
+      bgImage={image1}
+      bgImageAlt="bg1"
+      strength={200}
+      className={styles.introParallax}
+    >
+      <section className={styles.intro}>
         <h1>¿Qué es Roundnet Masters CR?</h1>
-      </div>
+      </section>
 
       <div className={`${styles.parallax} ${styles.bg1}`}>
         <h1>
@@ -17,10 +26,16 @@ const Intro = () => {
         </h1>
       </div>
 
-      <div className={`${styles.parallax} ${styles.bg2}`}>
+      <Parallax
+        blur={10}
+        bgImage={image2}
+        bgImageAlt="bg2"
+        strength={200}
+        className={`${styles.parallax} ${styles.bg2}`}
+      >
         <h1>Equipo Masters</h1>
-      </div>
-    </section>
+      </Parallax>
+    </Parallax>
   );
 };
 
