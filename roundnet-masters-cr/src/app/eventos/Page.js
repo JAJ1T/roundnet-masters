5b1ebb08d03ec594b82a526cc343afd948020ce8
AxselOSegueda <147475362+AxselOSegueda@users.noreply.github.com>
2023-11-23 05:32:32 -0600

    calendario finalizado con eventos de prueba

--- roundnet-masters-cr/src/app/eventos/Page.js
+++ roundnet-masters-cr/src/app/eventos/Page.js
@@ -1,28 +1,32 @@
+'use client'
 import React, { Component } from "react";
-import BigCalendar from "react-big-calendar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import "moment/locale/es"; // Moment.js localización en español
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "moment/locale/es.js";
 
-const localizer = BigCalendar.momentLocalizer(moment);
+// Configuración del localizador con momentLocalizer
+const localizer = momentLocalizer(moment);
+
+// Lista de eventos de ejemplo
 const myEventsList = [
-    {
-      title: "today",
-      start: new Date('2023-11-22T00:29:00'),
-      end: new Date('2023-11-22T01:29:00')
-    },
-    {
-      title: "Reunión de trabajo",
-      start: new Date('2023-11-23T09:00:00'),
-      end: new Date('2023-11-23T10:00:00')
-    }  
+  {
+    title: "Hoy",
+    start: new Date(), // La fecha y hora actuales
+    end: new Date(),
+  },
+  {
+    title: "Reunión de trabajo",
+    start: new Date("2023-11-23T09:00:00"),
+    end: new Date("2023-11-23T10:00:00"),
+  },
 ];
 
 class EventsCalendar extends Component {
   render() {
     return (
-      <div style={{ height: `${400}px` }} className="bigCalendar-container">
-        <BigCalendar
+      <div style={{ height: "400px" }} className="bigCalendar-container">
+        <Calendar
           localizer={localizer}
           events={myEventsList}
           startAccessor="start"
@@ -33,7 +37,7 @@ class EventsCalendar extends Component {
             today: "Hoy",
             month: "Mes",
             week: "Semana",
-            day: "Día"
+            day: "Día",
           }}
         />
       </div>
